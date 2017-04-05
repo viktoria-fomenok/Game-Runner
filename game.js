acecd4c8811a3e149e4a7ae8ac96df2f2ec5c003
Viktoria Fomenok <viky.fomenok@gmail.com>
2017-04-05 10:27:38 +0300

    create_object_ground

--- game.js
+++ game.js
@@ -2,89 +2,78 @@
  * Created by vikyf on 4/4/2017.
  */
 
-var pjs = new PointJS('2D', 1280 / 2, 720 / 2, { // 16:9
-    backgroundColor : '#53769A' // if need
+var pjs = new PointJS('2D', 1280 / 2, 720 / 2);
+var log    = pjs.system.log;
+var game   = pjs.game;
+var point  = pjs.vector.point;
+var camera = pjs.camera;
+var brush  = pjs.brush;
+var OOP    = pjs.OOP;
+var math   = pjs.math;
+var levels = pjs.levels;
+
+var width  = game.getWH().w;
+var height = game.getWH().h;
+
+pjs.system.setTitle('PointJS Game');
+
+// создание экземпляра движка
+var pjs = new PointJS('2d', 800, 400);
+pjs.system.initFullPage();
+
+var game = pjs.game;
+var point = pjs.vector.point;
+
+var fon1 = game.newImageObject({
+    x : 0, y : 0,
+    file : 'img/fon.jpg',
+    h : height,
+    onload : function () {
+        fon2.x = fon1.x+fon1.w;
+    }
 });
-// pjs.system.initFullPage(); // for Full Page mode
-// pjs.system.initFullScreen(); // for Full Screen mode (only Desctop)
 
-var log    = pjs.system.log;     // log = console.log;
-var game   = pjs.game;           // Game Manager
-var point  = pjs.vector.point;   // Constructor for Point
-var camera = pjs.camera;         // Camera Manager
-var brush  = pjs.brush;          // Brush, used for simple drawing
-var OOP    = pjs.OOP;            // Object's manager
-var math   = pjs.math;           // More Math-methods
-var levels = pjs.levels;         // Levels manager
+var fon2 = game.newImageObject({
+    x : 0, y : 0,
+    file : 'imgs/fon.jpg',
+    h : height
+});
 
-// var key   = pjs.keyControl.initKeyControl();
-// var mouse = pjs.mouseControl.initMouseControl();
-// var touch = pjs.touchControl.initTouchControl();
-// var act   = pjs.actionControl.initActionControl();
+var gr1 = game.newImageObject({
+    x : 0, y : 0,
+    file : 'imgs/ground.png',
+    w : width,
+    onload : function () {
+        gr2.y = gr1.y = height - gr1.h;
+        gr2.x = gr1.x+gr1.w;
+    }
+});
 
-var width  = game.getWH().w; // width of scene viewport
-var height = game.getWH().h; // height of scene viewport
+var gr2 = game.newImageObject({
+    x : 0, y : 0,
+    file : 'imgs/ground.png',
+    w : width
+});
 
-pjs.system.setTitle('PointJS Game'); // Set Title for Tab or Window
 
-// Game Loop
 game.newLoopFromConstructor('myGame', function () {
-    // Constructor Game Loop
 
     var myText = game.newTextObject({
-        positionC : point(game.getWH2().w, game.getWH2().h), // central position of text
-        size : 50, // size text
-        color : '#EAEAEA', // color text
-        text : 'Hello, World!', // label
-        alpha : 0, // alpha channel
-        font : 'Arial' // font family
+        positionC : point(game.getWH2().w, game.getWH2().h),
+        size : 50,
+        color : '#EAEAEA',
+        text : 'Hello, World!',
+        alpha : 0,
+        font : 'Arial'
     });
 
     this.update = function () {
-        // Update function
-
-        game.clear(); // clear screen
-
-        myText.draw(); // drawing text
-        myText.transparent(0.005); // change alpha [0..>..1]
+        game.clear();
+        myText.draw();
+        myText.transparent(0.005);
 
     };
 
-    // this.entry = function () { // [optional]
-    // 	// Entry Function
-    // 	log('myGame is started');
-    // };
-
-    // this.exit = function () { // [optional]
-    // 	// Exit function
-    // 	log('myGame is stopped');
-    // };
-
 });
-
-// // Advanced Game Loop
-// var MyGame = function () {
-// 	// Constructor Game Loop
-
-// 	this.update = function () {
-// 		// Update function
-// 	};
-
-// 	this.entry = function () {
-// 		// Entry Function
-// 	};
-
-// 	this.exit = function () {
-// 		// Exit function
-// 	};
-
-// };
-// game.newLoopFromClassObject('myGame', new MyGame());
-
-
-// Simple Game Loop
-// game.newLoop('myGame', function () {
-// 	// Update function
-// });
 
 game.startLoop('myGame');
